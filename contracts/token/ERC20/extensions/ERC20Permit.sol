7f5d61eef3485fdad9e606ae04bb8df853fbb97e
Jack Corddry <jackcorddry@gmail.com>
2022-08-12 20:56:18 -0700

    Naming Update

--- contracts/token/ERC20/extensions/ERC20Permit.sol
+++ contracts/token/ERC20/extensions/ERC20Permit.sol
@@ -3,9 +3,9 @@
 
 pragma solidity ^0.8.0;
 
-import "./draft-IERC20Permit.sol";
+import "./IERC20Permit.sol";
 import "../ERC20.sol";
-import "../../../utils/cryptography/draft-EIP712.sol";
+import "../../../utils/cryptography/EIP712.sol";
 import "../../../utils/cryptography/ECDSA.sol";
 import "../../../utils/Counters.sol";
 
